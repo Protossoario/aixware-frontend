b5712b1ee90c2a0973824b18d61d464f9e63dd65
Lalo <lalo.sa.94@gmail.com>
2017-02-10 14:48:10 -0600

    Fixed address not loading when navigating to component, broke address loading when accessing component directly

--- src/app/geocode.service.ts
+++ src/app/geocode.service.ts
@@ -1,29 +1,34 @@
 import { Injectable, NgZone } from '@angular/core';
 import { GoogleMapsAPIWrapper } from 'angular2-google-maps/core';
 import { MapsAPILoader } from 'angular2-google-maps/core';
-import { Observable, Observer } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable()
 export class GeocodeService extends GoogleMapsAPIWrapper {
+  geocoder$ = new Subject<any>();
 
   constructor(private __loader: MapsAPILoader, private __zone: NgZone) {
     super(__loader, __zone);
+    __loader.load().then(() => {
+      let newGeocoder = new google.maps.Geocoder;
+      this.geocoder$.next(newGeocoder);
+    });
   }
 
   getAddress(latitude: number, longitude: number): Observable<any> {
-    let geocoder = new google.maps.Geocoder;
-    let latLng = new google.maps.LatLng(latitude, longitude);
-    return Observable.create((observer: Observer<string>) => {
+    let address$ = new Subject;
+    this.geocoder$.subscribe((geocoder: google.maps.Geocoder) => {
+      let latLng = new google.maps.LatLng(latitude, longitude);
       geocoder.geocode({ 'location': latLng }, (results, status) => {
         if (status === google.maps.GeocoderStatus.OK) {
-          observer.next(results[0].formatted_address);
-          observer.complete();
+          address$.next(results[0].formatted_address);
         } else {
           console.error('Error - ', results, ' & Status - ', status);
-          observer.error(status);
+          address$.error(status);
         }
       });
-    });
+    })
+    return address$;
   }
 
 }
